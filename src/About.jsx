b174a11fa965191c9fcbe63100ba43e60d4bd386
Arun-hash-akr <arunsankarramav@gmail.com>
2026-07-09 16:42:24 +0530

    About.jsx
    Updated

--- src/About.jsx
+++ src/About.jsx
@@ -11,17 +11,11 @@ const About = () => {
 
                 <section class="about-text">
                     <p>
-                        I'm Creative Director and UI/UX Designer from Sydney, Australia, working in web development and print media.
-                        I enjoy
-                        turning complex problems into simple, beautiful and intuitive designs.
+                    I'm a Frontend Developer and Web Designer from Tamil Nadu, working with HTML, CSS, JavaScript, and React JS. I enjoy turning creative ideas into responsive, attractive, and user-friendly websites.
                     </p>
 
                     <p>
-                        My job is to build your website so that it is functional and user-friendly but at the same time attractive.
-                        Moreover, I
-                        add personal touch to your product and make sure that is eye-catching and easy to use. My aim is to bring
-                        across your
-                        message and identity in the most creative way. I created web design for many famous brand companies.
+                   My goal is to build websites that are functional, responsive, and easy to use while keeping them visually appealing. Moreover, I enjoy learning new technologies, improving my frontend development skills, and creating clean web designs. My aim is to deliver modern websites that provide a great user experience and reflect creativity. I have created several personal and academic web projects to improve my skills as a frontend developer. goal is to build websites that are functional, responsive, and easy to use while keeping them visually appealing. Moreover, I enjoy learning new technologies, improving my frontend development skills, and creating clean web designs. My aim is to deliver modern websites that provide a great user experience and reflect creativity. I have created several personal and academic web projects to improve my skills as a frontend developer.
                     </p>
                 </section>
 
